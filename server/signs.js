8fcf75042273ca946f27953b184b430ecfc77a25
Jope Algorta <jpalgorta@correo.um.edu.uy>
2021-09-27 20:29:21 -0300

    Reproduce audio with sign name (It doesn't work in Chrome)

--- server/signs.js
+++ server/signs.js
@@ -2,11 +2,13 @@ module.exports = [
     {
         sign: 1,
         name: "PARE",
-        image: "/images/stop.png"
+        image: "/images/stop.png",
+        audio: "/audio/stop.mp3"
     },
     {
         sign: 2,
         name: "CEDA EL PASO",
-        image: "/images/ceda.png"
+        image: "/images/ceda.png",
+        audio: "/audio/ceda.mp3"
     }
 ]
