38033aa78501bf6eabd8b0706f5232e2c59d6ab9
Jose Algorta <jpalgor@correo.um.edu.uy>
2021-10-31 23:17:33 -0300

    Allow Loader to be different size.

--- server/client/src/components/Loader.js
+++ server/client/src/components/Loader.js
@@ -2,10 +2,18 @@ import React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 
-const Loader = () => {
+const Loader = ({ size }) => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', height: '100%', alignItems: 'center' }}>
-      <CircularProgress />
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100%',
+        alignItems: 'center',
+        padding: '5px',
+      }}
+    >
+      <CircularProgress size={size || 40} />
     </Box>
   );
 };
