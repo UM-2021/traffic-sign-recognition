ac2808e5e1503fe723b945abadb7c4ad41805b8c
Jope Algorta <jpalgorta@correo.um.edu.uy>
2021-11-07 18:45:33 -0300

    request to vm with auth wip

--- worker/gpscoordenadas.py
+++ worker/gpscoordenadas.py
@@ -8,17 +8,18 @@ def getCoords():
 	ser=serial.Serial(port, baudrate=9600, timeout=10)
 	dataout = pynmea2.NMEAStreamReader()
 	newdata=ser.readline()
-	print(newdata)
+	# print(newdata)
 	# newdata=newdata.decode("utf-8")
 	try:
 		# print(newdata.decode("utf-8"))
 		newdata=newdata.decode("utf-8")
+		# print(newdata)
 	except:
 		pass
-	print(newdata)
+	# print(newdata)
 	if newdata[0:6] == "$GPRMC":
 		newmsg=pynmea2.parse(newdata)
-		print(newmsg)
+		# print(newmsg)
 		lat=newmsg.latitude
 		lng=newmsg.longitude
 		gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
@@ -28,5 +29,5 @@ def getCoords():
 	else:
 		return 0,0 # temporal para testear que no me estaba agarrando las coordenadas
 
-while True:
-	print(getCoords())
+#while True:
+#	print(getCoords())
